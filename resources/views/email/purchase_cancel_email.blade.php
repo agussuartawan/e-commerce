847486aaa3dc435463f75fe7134cab8d4bd1dcf3
unknown <Agus Suartawan>
2022-05-13 10:13:48 +0800

    perbaikan kecil

--- resources/views/email/purchase_cancel_email.blade.php
+++ resources/views/email/purchase_cancel_email.blade.php
@@ -1,6 +1,7 @@
 <h3>Halo, {{ $name }}</h3>
 
 <p>Pesanan anda dengan nomor <strong>{{ $order_number }}</strong> telah dibatalkan karena anda belum melakukan
-    pembayaran. Anda dapat melakukan pemesanan kembali pada situs web resmi CV. Murni Sejati melalui <a href="{{ env('APP_URL') }}">link berikut</a></p>
+    pembayaran. Anda dapat melakukan pemesanan kembali pada situs web resmi CV. Murni Sejati melalui <a
+        href="{{ env('APP_URL') }}">www.murnisejati.host</a></p>
 <br>
 <p>Terima Kasih</p>
